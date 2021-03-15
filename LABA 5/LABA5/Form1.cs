5fd060265ae6d8f65784a0fdbb79b17ac9a0e818
Maxim Bessonov <bessonov_mp@mail.ru>
2021-03-15 18:03:30 +0300

    Full

---
+++ LABA 5/LABA5/Form1.cs
@@ -0,0 +1,36 @@
+﻿//http://www.dneonline.com/calculator.asmx
+
+using System;
+using System.Windows.Forms;
+
+namespace LABA5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Calc.CalculatorSoap client = new Calc.CalculatorSoapClient();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = client.Add(int.Parse(textBox1.Text), int.Parse(textBox2.Text)).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = client.Divide(int.Parse(textBox1.Text), int.Parse(textBox2.Text)).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Text = client.Multiply(int.Parse(textBox1.Text), int.Parse(textBox2.Text)).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label2.Text = client.Subtract(int.Parse(textBox1.Text), int.Parse(textBox2.Text)).ToString();
+        }
+    }
+}

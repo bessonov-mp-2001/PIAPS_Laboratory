8fc08dea3d13579fc1a070974c286e5683e43164
Maxim Bessonov <bessonov_mp@mail.ru>
2021-03-18 19:18:57 +0300

    added 4 labs

---
+++ LABA 4/XMLRPCClient/XMLRPCClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace XMLRPCClient
+{
+    class Program
+    {
+        static ServObj obj;
+
+        static void Main(string[] args)
+        {
+            obj = new ServObj("http://127.0.0.1:8301");
+            ArrayList temp_arr = null;
+            ArrayList arr = new ArrayList();
+            Console.Write("Введите размер квадратной матрицы: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите квадратную матрицу: ");
+            string[] str_arr;
+            for (int i = 0; i < size; ++i)
+            {
+                str_arr = (Console.ReadLine()).Split(' ');
+                for (int j = 0; j < size; j++)
+                    arr.Add(Convert.ToInt32(str_arr[j]));
+            }
+            Console.Clear();
+            Console.WriteLine("Введенная матрица:\n");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                    Console.Write(arr[j + i * size] + "\t");
+                Console.WriteLine("\n");
+            }
+            temp_arr = obj.Matrix(arr, size); //отправить запрос
+            Console.WriteLine("\nМинимальный элемент: " + temp_arr[size * size] + "\n");
+            Console.WriteLine("Итоговая матрица:\n");
+            temp_arr.RemoveAt(size * size);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                    Console.Write(temp_arr[j + i * size] + "\t");
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LABA 4/XMLRPCServer/XMLRPCServer/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using Nwc.XmlRpc;
+
+namespace XMLRPCServer
+{
+    class Server
+    {
+        const int port = 8301;
+
+        static void Main(string[] args)
+        {
+            XmlRpcServer server = new XmlRpcServer(port);
+            server.Add("sample", new Server());
+            Console.WriteLine("Сервер начал работу");
+            server.Start(); //Запуск сервера
+        }
+
+        [XmlRpcExposed]
+        public ArrayList Matrix(ArrayList arr, int size)
+        {
+            int min = (int)arr[0];
+            int i, j;
+            int a = 0;
+            int b = 0;
+            for (j = 0; j < size; j++)
+            {
+                for (i = 0; i < size; i++)
+                {
+                    if ((int)arr[i + j * size] <= min)
+                    {
+                        a = i;
+                        b = j;
+                        min = (int)arr[i + j * size];
+                    }
+                }
+            }
+            if (a >= b)
+            {
+                a = a - b;
+                b = 0;
+                do
+                {
+                    arr[a + b * size] = 0;
+                    a += 1;
+                    b += 1;
+                } while ((a + b * size) % size != 0);
+            }
+            else
+            {
+                b = b - a;
+                a = 0;
+                do
+                {
+                    arr[a + b * size] = 0;
+                    a += 1;
+                    b += 1;
+                } while (a != size);
+            }
+            bool flag = false;
+            for (int k = size - 1; k > 0; k--) //Возведение в квадрат элементов ниже необходимой диагонали с левого нижнего угда
+            {
+                if (flag)
+                    break;
+                i = k;
+                j = 0;
+                while (i < size)
+                {
+                    if ((int)arr[j + i * size] == 0)
+                    {
+                        flag = true;
+                        break;
+                    }
+                    arr[j + i * size] = (int)arr[j + i * size] * (int)arr[j + i * size];
+                    i++;
+                    j++;
+                    if (j == size)
+                        break;
+                }
+            }
+            j = 0;
+            for (i = 0; i < size; i++) //Заполнение нулями элементов необходимой диагонали
+            {
+                if (flag)
+                    break;
+                if ((int)arr[j + i * size] == 0)
+                {
+                    flag = true;
+                    break;
+                }
+                arr[j + i * size] = (int)arr[j + i * size] * (int)arr[j + i * size];
+                j++;
+            }
+            arr.Add(min); //Добавить в самом конце минимальный элемент
+            return arr;
+        }
+    }
+}

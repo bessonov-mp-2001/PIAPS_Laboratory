59fddca1cdbc1ceffec1f1b45fe0f57da1bc70da
Maxim Bessonov <bessonov_mp@mail.ru>
2021-04-12 14:56:40 +0300

    modified version

--- LABA 8/COM_OBJECT/COM_OBJECT/Calculator.cs
+++ LABA 8/COM_OBJECT/COM_OBJECT/Calculator.cs
@@ -1,24 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-
-[Guid("364C5E66-4412-48E3-8BD8-7B2BF09E8922")]
-[ComVisible(true)]
-public interface ICalculator
-{
-    int Sum(int a, int b);
-    int Divide(int a, int b);
-    int Substract(int a, int b);
-    int Multiply(int a, int b);
-}
 
 [Guid("8C034F6A-1D3F-4DB8-BC99-B73873D8C297")]
 [ClassInterface(ClassInterfaceType.None)]
 [ComVisible(true)]
-public class Calculator : ICalculator
+public class Calculator : COM_OBJECT.ICalculator
 {
     public int Sum(int a, int b)
     {
---
+++ LABA 8/COM_OBJECT/COM_OBJECT/ICalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace COM_OBJECT
+{
+    [Guid("364C5E66-4412-48E3-8BD8-7B2BF09E8922")]
+    [ComVisible(true)]
+    public interface ICalculator
+    {
+        int Sum(int a, int b);
+        int Divide(int a, int b);
+        int Substract(int a, int b);
+        int Multiply(int a, int b);
+    }
+}
